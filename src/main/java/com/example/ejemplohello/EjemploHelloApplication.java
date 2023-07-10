b05ec7e0d67560fd35de8119041dc3b6dd2ea92a
Anticona23 <134670116+Anticona23@users.noreply.github.com>
2023-07-10 01:58:03 +0000

    Completo con target

---
+++ src/main/java/com/example/ejemplohello/EjemploHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejemplohello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejemplohello/EjemploHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemplohello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
